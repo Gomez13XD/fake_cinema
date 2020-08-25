4a57d1e5a95c436f927b882e1f1b4350c50e085d
Andres <andresgomezcedres@hotmail.com>
2020-08-25 16:27:47 -0300

    Proyecto con estilos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {User} from '../login/user.class';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +12,9 @@ import {User} from '../login/user.class';
 export class LoginPage implements OnInit {
 
   user: User = new User();
+  loginImg = 'https://via.placeholder.com/500';
 
-  constructor(private router: Router, private authSrv: AuthService) { }
+  constructor(private router: Router, private authSrv: AuthService, public alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -20,8 +22,39 @@ export class LoginPage implements OnInit {
   async login(){
     const user = await this.authSrv.login(this.user);
     if (user){
-      this.router.navigateByUrl('/home');
+      this.router.navigateByUrl('/movies');
     }
+  }
+
+  async changeImg(){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'New login image!',
+      message: 'Insert a new image url',
+      inputs: [
+        {
+          name: 'name3',
+          value: this.loginImg,
+          type: 'url',
+          placeholder: 'https://myimg.com'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+          }
+        }, {
+          text: 'Okay',
+          handler: (alertData) => {
+            this.loginImg = alertData.name3;
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
